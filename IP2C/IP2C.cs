6be62526922a9b237272a8e736ea0029cc0a6b58
Isuru Bandaranayake <isbana.git@gmail.com>
2019-09-18 23:03:57 +0530

    Initial commit

---
+++ IP2C/IP2C.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace IP2C
+{
+    public class IP2C
+    {
+        private LinkedList<Record> records = new LinkedList<Record>();
+
+        public IP2C(string path)
+        {
+            foreach (var line in File.ReadAllLines(path))
+            {
+                if (line.StartsWith("#"))
+                {
+                    continue;
+                }
+                else
+                {
+                    string[] splitted = line.Replace("\"", "").Split(',');
+                    records.AddLast(new Record(uint.Parse(splitted[0]), uint.Parse(splitted[1]), splitted[2],
+                        double.Parse(splitted[3]), splitted[4], splitted[5], splitted[6]));
+                }
+            }
+        }
+
+        public uint GetDecIP(string ipv4)
+        {
+            byte[] bytes = IPAddress.Parse(ipv4).GetAddressBytes();
+            Array.Reverse(bytes);
+            return BitConverter.ToUInt32(bytes, 0);
+        }
+
+        public Record GetDetails(string ipv4)
+        {
+            uint ipv4Dec = GetDecIP(ipv4);
+            foreach (Record record in records)
+            {
+                if (record.ipFrom <= ipv4Dec && ipv4Dec <= record.ipTo)
+                {
+                    return record;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ IP2C/Record.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IP2C
+{
+    public class Record
+    {
+        public Record(uint ipFrom, uint ipTo, string registry, double assigned, string ctry, string cntry, string country)
+        {
+            this.ipFrom = ipFrom;
+            this.ipTo = ipTo;
+            this.registry = registry;
+            this.assigned = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(assigned);
+            this.ctry = ctry;
+            this.cntry = cntry;
+            this.country = country;
+        }
+        public uint ipFrom { get; }
+        public uint ipTo { get; }
+        public string registry { get; }
+        public DateTime assigned { get; }
+        public string ctry { get; }
+        public string cntry { get; }
+        public string country { get; }
+
+    }
+}
